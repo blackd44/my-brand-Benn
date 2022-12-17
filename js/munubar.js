b53fd4dcbd8097f738feacdf05f71df2a03b3d64
blackd44 <irabd44@gmail.com>
2022-12-17 09:28:38 +0200

    fixing the bugs

--- js/munubar.js
+++ js/munubar.js
@@ -1,19 +1,64 @@
 let munubar = document.querySelector('header .menu')
+let header = document.querySelector('header')
 let nav = document.querySelector('header ul#head')
+let body = document.querySelector('body')
+const ua = navigator.userAgent;
+
 
 let menuActive = false
+let navtra = '90vw'
+
+function checkWidth() {
+    if (
+        (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) &&
+        (window.outerWidth > 1366)
+    ) {
+        nav.style.translate = '0'
+        header.style.position = 'absolute !important'
+    }
+    else if (
+        !(/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) &&
+        (window.outerWidth > 800)
+    ) {
+        nav.style.translate = '0'
+        header.style.position = 'absolute !important'
+    }
+    else {
+        nav.style.translate = navtra
+        header.style.position = 'fixed'
+    }
+    console.log(window.innerWidth)
+    if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+        if (window.outerWidth > 700) {
+            body.style.fontSize = '120%'
+        }
+        else if (window.outerWidth > 450) {
+            body.style.fontSize = '150%'
+        }
+        else {
+            body.style.fontSize = '180%'
+        }
+        //console.log(window.outerWidth)
+    }
+}
+
+checkWidth()
 
 munubar.addEventListener('click', e => {
     if (!menuActive) {
         munubar.classList.add('active')
         menuActive = true
-        nav.style.translate = '0'
-        nav.parentNode.style.position = 'fixed'
+        navtra = '0'
+        checkWidth()
     }
     else {
         munubar.classList.remove('active')
         menuActive = false
-        nav.style.translate = '90vw'
-        nav.parentNode.style.position = 'absolute'
+        navtra = '90vw'
+        checkWidth()
     }
+})
+
+window.addEventListener('resize', e => {
+    checkWidth()
 })
