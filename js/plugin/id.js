7054f4b44bbc2789a969c69d99370e72ce80207f
blackd44 <irabd44@gmail.com>
2023-01-01 22:12:20 +0200

    fixing the bug in creating new id

--- js/plugin/id.js
+++ js/plugin/id.js
@@ -1,10 +1,11 @@
 export default function newId(arr) {
     let val = undefined
+    let sorted = [...arr].sort((a, b) => Number(b.id) - Number(a.id))
     if (arr.length === 0) {
         val = 0
     }
     else {
-        val = Number(arr[0].id) + 1
+        val = Number(sorted[0].id) + 1
     }
     return (val + '').padStart(6, '0')
 }
