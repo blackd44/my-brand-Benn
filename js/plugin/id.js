3347eb1fa725c4646f6012d2376a1e1689afd8fe
blackd44 <irabd44@gmail.com>
2023-01-01 22:26:09 +0200

    fixing the bug in creating new id

--- js/plugin/id.js
+++ js/plugin/id.js
@@ -1,11 +1,23 @@
+function convertToBase(number, fromBase, toBase) {
+
+    let decimalNumber = parseInt(number, fromBase)
+
+    return decimalNumber.toString(toBase)
+}
+
+const IDBASE = 36
+const INITBASE = 10
+const IDLENGTH = 6
+const IDCHAR = '0'
+
 export default function newId(arr) {
     let val = undefined
-    let sorted = [...arr].sort((a, b) => Number(b.id) - Number(a.id))
+    let sorted = [...arr].sort((a, b) => convertToBase(b.id, IDBASE, INITBASE) - convertToBase(a.id, IDBASE, INITBASE))
     if (arr.length === 0) {
         val = 0
     }
     else {
-        val = Number(sorted[0].id) + 1
+        val = convertToBase((convertToBase(sorted[0].id, IDBASE, INITBASE) + 1), INITBASE, IDBASE)
     }
-    return (val + '').padStart(6, '0')
+    return (val + '').padStart(IDLENGTH, IDCHAR)
 }
